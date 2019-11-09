e5979a3ce0de0f46a472942beee73a80e46fec50
Alexandre Geraldo <ageraldo@itktechnologies.com>
2019-11-09 16:56:10 -0500

    updates

---
+++ xamarin/WeatherApp/WeatherApp.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 3e16fa5da28b89c13a9024f7f113f391f888650d */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1
+	/* entry-length */
+	.word	99
+	/* value-offset */
+	.word	49
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 100
+	.include	"typemap.jm.inc"
---
+++ xamarin/WeatherApp/WeatherApp.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 140702e486435fae995e6b43c2e735c6d005a122 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1
+	/* entry-length */
+	.word	99
+	/* value-offset */
+	.word	50
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 100
+	.include	"typemap.mj.inc"
